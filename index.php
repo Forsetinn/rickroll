90b8e3aa59a0cef6a6f531a182bc1aed52d1ad0b
Forsetinn <eidurgeir@gmail.com>
2022-11-20 20:49:36 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<style>
+  .video-container {
+    position:relative;
+    padding-bottom:100%;
+    badding-top:100%;
+    height:0;
+    overflow:hidden;
+  }
+  
+  .video-container iframe {
+    position:absolute;
+    top:0;
+    left:0;
+    width:100%;
+    height:100%;
+  }
+</style>
+
+<div class="video-container">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
